6506ea89b663471efbcc9509eb812759bd0add97
UmezuPenguin <ayoanuzemu.work.2240@gmail.com>
2019-11-20 15:49:28 +0900

    added activities for bluetooth host and client

---
+++ app/src/main/java/com/example/nullsing_kenta/MatchingHostActivity.java
@@ -0,0 +1,14 @@
+package com.example.nullsing_kenta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchingHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching_host);
+    }
+}
--- app/src/main/java/com/example/nullsing_kenta/MathcingActivity.java
+++ app/src/main/java/com/example/nullsing_kenta/MathcingActivity.java
@@ -56,4 +56,6 @@ public class MathcingActivity extends Activity {
             startActivity(intent);
         }
     }
+
+
 }
